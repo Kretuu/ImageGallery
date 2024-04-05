54e65148640b0e980bc292beb907337bffd69e0c
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2024-04-05 12:58:52 +0200

    Create sign-up and login pages, sign out functionality and add alert to main app template.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_30_073346) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_05_070419) do
   create_table "photo_galleries", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "name"
@@ -34,6 +34,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_30_073346) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
