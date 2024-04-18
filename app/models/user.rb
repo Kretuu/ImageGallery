d35a8f60266b876421ac498d85fba3d407767fc6
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2024-04-18 22:16:36 +0100

    Allow editing user and deleting user account. Also create simple UI for this.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
+  has_many :photo_galleries, dependent: :destroy
 
   def name
     "#{first_name} #{last_name}"
