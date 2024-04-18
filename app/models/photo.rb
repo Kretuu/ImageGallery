614ee9bcc7491b420beba9d74b9c3c0e980dc451
Jakub Kreczetowski <jkreczetowski1@sheffield.ac.uk>
2024-04-18 21:38:38 +0100

    Handle removing photo if it has been set as cover photo of a gallery

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,9 @@
 class Photo < ApplicationRecord
   belongs_to :photo_gallery
   has_one_attached :original_image
+
   after_commit :process_images
+  before_destroy :remove_thumbnail_references
 
   def image
     original_image.variant(crop: [x, y, w, h])
@@ -15,5 +17,11 @@ class Photo < ApplicationRecord
   protected
   def process_images
     ImageProcessingJob.perform_later(self)
+  end
+
+  def remove_thumbnail_references
+    PhotoGallery.where(thumbnail: self).each do |gallery|
+      gallery.update(thumbnail: nil)
+    end
   end
 end
